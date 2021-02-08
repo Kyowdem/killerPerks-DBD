5fa0ba94d5ed4dce9b7176336a5d57130169b4a8
Collomb Mehdi <collomb.mehdi@gmail.com>
2021-02-07 23:02:13 -0500

    recup index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,32 @@
-import {Button, TextView, contentView} from 'tabris';
+import { TextView, contentView, TextInput } from 'tabris';
+import { Cell, ListView } from 'tabris-decorators';
+import { getJSON } from './JSON.js';
+import { addPerk, MAJPerk } from './perk.js';
+import { autoCompletion, setListView, printAutoCompletion, autoCompletionToList } from './listView.js';
+
+
+const defaultApp = function () {
+  MAJPerk();
+  visibility(0);
+  // killerPerksJSON = getJSON();
+}
+
+const inputPerk = new TextInput({
+  top: 24,
+  left: '15%',
+  right: '15%',
+  message: "Entrer le nom d'un perk"
+}).onAccept(function () { addPerk(this.text) }).onInput(() => (inputPerk.text.length >= 3) ? printAutoCompletion() : setListView());
+
 
 contentView.append(
-  <$>
-    <Button center onSelect={showText}>Tap here</Button>
-    <TextView centerX bottom='prev() 20' font='24px'/>
-  </$>
+  inputPerk,
+  <ListView background="#FFD400" left="60" top="90" right="60" bottom="50"
+    stretch onSelect={autoCompletionToList} items={autoCompletion()}>
+    <Cell selectable padding={6} height={25}>
+      <TextView centerY bind-text='item' font='12px' />
+    </Cell>
+  </ListView>
 );
 
-function showText() {
-  $(TextView).only().text = 'Tabris.js rocks!';
-}
+defaultApp();
