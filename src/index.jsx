798086c0ccedbbbd5196d60d31a4a72a8c324f5b
Collomb Mehdi <collomb.mehdi@gmail.com>
2021-02-26 18:18:41 -0500

    create new js

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import { CollectionView, Composite, contentView, TextView, TextInput, AlertDialog } from 'tabris';
 import { Cell, ListView } from 'tabris-decorators';
 import { killerPerksJSON, putJSON } from './components/JSON'
+import { printAutoCompletion, addNewItem, visibility } from './components/input'
 
 
 // Added perk after confirm typing
@@ -36,32 +37,6 @@ function popup(text) {
     title: text,
     buttons: { ok: "Ok" }
   }).open();
-}
-
-function addNewItem(perk) {
-  if (addedPerks.some(x => x == perk)) {
-    popup(`The perk "${perk}" has already been added`);
-  }
-  else if (addedPerks.length < 4) {
-    addedPerks.push(perk.toLowerCase());
-    $("CollectionView").first().refresh();
-    $("CollectionView").first().insert(0);
-  }
-  // Print message after 4 perk added
-  if (addedPerks.length > 3) {
-    popup('Les 4 perk seront ajoutés');
-    putJSON(addedPerks);
-    
-    // Empty after added it to cloud
-    for (let i = 0; i < 4; i++) $(CollectionView).last().remove(0);
-    addedPerks = [];
-  }
-
-
-  // Stop focused input
-  $('TextInput').first().focused = false;
-  $('TextInput').first().text = "";
-  visibility(0);
 }
 
 function createCell() {
@@ -123,39 +98,4 @@ async function animateCancel(target) {
 
 function direction(offset) {
   return offset ? offset < 0 ? -1 : 1 : 0;
-}
-
-// Return an array of autocomplete of input user 
-function autoCompletion(perk) {
-  if (perk == "") return [];
-  var autoPerk = [];
-
-  for (let prop in killerPerksJSON) {
-    if (prop.substring(0, perk.length) == perk)
-      if (autoPerk.length <= 5)
-        autoPerk.push(prop);
-  }
-  return autoPerk;
-}
-
-// Print autocompletion
-function printAutoCompletion(perk) {
-  if (perk.length >= 3)
-    setListView(autoCompletion(perk));
-  else
-    setListView();
-}
-
-// set list with an array and refresh it
-function setListView(list = []) {
-  if (list.length) visibility(1);
-  else visibility(0);
-
-  $(ListView).first().items = list;
-  $(ListView).first().refresh();
-}
-
-// show/hide ListView
-function visibility(visible = 1) {
-  $(ListView).first().bottom = (visible ? '50' : '100%');
 }
